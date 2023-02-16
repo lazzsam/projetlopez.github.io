9dcdb2f846657a6a22c6e1a6ad7d985ba403bcd6
lazzsam <55142358+lazzsam@users.noreply.github.com>
2023-02-16 14:11:27 +0100

    commit projet lopez

---
+++ La boite a lopez/main.js
@@ -0,0 +1,427 @@
+$(document).ready(function () {
+  function loading(btn) {
+    // Loading circle while playing
+  }
+
+  // btn 1.1
+  $("#jaiDisQue").click(function () {
+    let audio = new Audio("./desPd.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("J'ai dis que vous etiez...");
+    }, 1500);
+  });
+
+  // btn 1.2
+  $("#yeah").click(function () {
+    let audio = new Audio("yeah.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Yeah yeah ...");
+    }, 2200);
+  });
+  // btn 1.3
+  $("#pokos").click(function () {
+    let audio = new Audio("pokos.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Street n'air force n'pokos");
+    }, 3000);
+  });
+  // btn 1.4
+  $("#buthead").click(function () {
+    let audio = new Audio("buthead.mp3");
+    audio.play();
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Butthead");
+    }, 3000);
+  });
+  // btn 1.5
+  $("#cousinEric").click(function () {
+    let audio = new Audio("cousinEric.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Cousin à Eric");
+    }, 4000);
+  });
+  // btn 2.1
+  $("#regardezLe").click(function () {
+    let audio = new Audio("regardezLe.mp3");
+    audio.play();
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Regardez le lui");
+    }, 6500);
+  });
+
+  // btn 2.2
+  $("#divague").click(function () {
+    let audio = new Audio("divague.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Je divague");
+    }, 2000);
+  });
+
+  // btn 2.3
+  $("#beatAfricain").click(function () {
+    let audio = new Audio("africain.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Sur un beat africain ");
+    }, 3000);
+  });
+
+  // btn 2.4
+  $("#dorsPasIci").click(function () {
+    let audio = new Audio("jdorsPasIci.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("J'dors pas içi !");
+    }, 1000);
+  });
+
+  // btn 2.5
+  $("#mongol1").click(function () {
+    let audio = new Audio("mongol1.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("???!");
+    }, 3000);
+  });
+
+  // btn 3.1
+  $("#dingueriz").click(function () {
+    let audio = new Audio("dingueriz.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Dingueriz");
+    }, 3000);
+  });
+
+  // btn 3.2
+  $("#hmRuler").click(function () {
+    let audio = new Audio("hmRuler.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Hm Ruler !");
+    }, 4000);
+  });
+
+  // btn 3.3
+  $("#pickDeDeft").click(function () {
+    let audio = new Audio("pickDeDeft.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Pick de deeeft");
+    }, 3000);
+  });
+
+  // btn 3.4
+  $("#mongol2").click(function () {
+    let audio = new Audio("mongol2.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("???!! #2");
+    }, 1800);
+  });
+
+  // btn 3.5
+  $("#hahaha").click(function () {
+    let audio = new Audio("hahaha.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Hahahaha");
+    }, 3000);
+  });
+
+  // btn 4.1
+  $("#arbre").click(function () {
+    let audio = new Audio("arbre.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("I am un arbre");
+    }, 2000);
+  });
+
+  // btn 4.2
+  $("#mbappos").click(function () {
+    let audio = new Audio("mbappos.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Mbappos");
+    }, 5000);
+  });
+
+  // btn 4.3
+  $("#cRuler").click(function () {
+    let audio = new Audio("cRuler.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Nan c'est Ruler ♫");
+    }, 5900);
+  });
+
+  // btn 4.4
+  $("#potoMirs").click(function () {
+    let audio = new Audio("potoMirs.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Poto mirs");
+    }, 3000);
+  });
+
+  // btn 4.5
+  $("#drJteB").click(function () {
+    let audio = new Audio("drBaise.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Oho dr j'te baise");
+    }, 1800);
+  });
+
+  // btn 5.1
+  $("#hahahihi").click(function () {
+    let audio = new Audio("hahahihi.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Hahaha hihihi");
+    }, 4300);
+  });
+
+  // btn 5.2
+  $("#csc").click(function () {
+    let audio = new Audio("csc.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("CsC de zinzin...");
+    }, 6300);
+  });
+
+  // btn 5.3
+  $("#jsuisDz").click(function () {
+    let audio = new Audio("jsuisDz.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("J'suis dz");
+    }, 1500);
+  });
+
+  // btn 5.4
+  $("#mongol3").click(function () {
+    let audio = new Audio("mongol3.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("???!! #3");
+    }, 4600);
+  });
+
+  // btn 5.5
+  $("#coreen").click(function () {
+    let audio = new Audio("coreen.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Coréen");
+    }, 5000);
+  });
+
+  // btn 6.1
+  $("#twipple").click(function () {
+    let audio = new Audio("twipple.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Twipple kill !");
+    }, 3000);
+  });
+
+  // btn 6.2
+  $("#arreteFilmer").click(function () {
+    let audio = new Audio("arreteFilmer.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Arrete de filmer wesh");
+    }, 2200);
+  });
+
+  // btn 6.3
+  $("#babale").click(function () {
+    let audio = new Audio("babale.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("La baballe");
+    }, 1500);
+  });
+
+  // btn 6.4
+  $("#faker").click(function () {
+    let audio = new Audio("faker.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("?? T1 Faker");
+    }, 5500);
+  });
+
+  // btn 6.5
+  $("#worlds22").click(function () {
+    let audio = new Audio("worlds22.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Worlds 2022");
+    }, 5000);
+  });
+
+  // btn 7.1
+  $("#pizza").click(function () {
+    let audio = new Audio("maPizza.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("En parlant d'ma pizza ...");
+    }, 5000);
+  });
+
+  // btn 7.2
+  $("#coreen2").click(function () {
+    let audio = new Audio("coreen2.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Coréen #2");
+    }, 5900);
+  });
+
+  // btn 7.3
+  $("#leDaron").click(function () {
+    let audio = new Audio("leDaron.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Il pèse combien ton daron ?");
+    }, 1500);
+  });
+
+  // btn 7.4
+  $("#begaye").click(function () {
+    let audio = new Audio("begaye.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Beguaye");
+    }, 2000);
+  });
+
+  // btn 7.5
+  $("#aplKeuf").click(function () {
+    let audio = new Audio("aplKeuf.mp3");
+    audio.play();
+
+    // Loader while playing
+    $(this).html('<div class="loader"></div>');
+    setTimeout(() => {
+      $(this).html("Les keufs j'vais appeller !");
+    }, 1500);
+  });
+  // ------------------------------------------------------------------
+
+  $("#fb").click(function () {
+    alert("Plus personne n'utilise facebook...");
+  });
+
+  // ------------------------------------------------------------------
+});
+// test
